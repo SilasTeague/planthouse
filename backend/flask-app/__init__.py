5f4599c6d6b4d833620ef8afe7e3fa7428c9efc0
Silas Teague <silasteague@gmail.com>
2024-10-21 18:33:35 -0400

    Restructure __init__.py to use app context and move route handling to app.py

--- backend/flask-app/__init__.py
+++ backend/flask-app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def home():
-        return "Welcome to PlantHouse!"
+    with app.app_context():
+        from . import app
     
+
     return app
