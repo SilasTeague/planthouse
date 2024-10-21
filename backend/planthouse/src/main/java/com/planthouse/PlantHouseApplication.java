d8b3f4527d6d7d654e9280b0f552d423b62c836a
Silas Teague <silasteague@gmail.com>
2024-10-20 20:39:38 -0400

    Initialize Spring Boot project with Spring Web dependency

---
+++ backend/planthouse/src/main/java/com/planthouse/PlantHouseApplication.java
@@ -0,0 +1,13 @@
+package com.planthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantHouseApplication.class, args);
+	}
+
+}
---
+++ backend/planthouse/src/test/java/com/planthouse/PlantHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
