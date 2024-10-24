6b8cbde7aa1233e0b9a5e99293f1ce8f08a20152
Silas Teague <silasteague@gmail.com>
2024-10-24 16:47:05 -0400

    Configure SQLite database, initialize SQLAlchemy, add .gitignore

--- backend/flask-app/__init__.py
+++ backend/flask-app/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy as sa
+from flask_migrate import Migrate
+
+# Initialize the database
+db = sa()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+
+    # Database configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'  # Or use PostgreSQL URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize database and migration tool
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
         from . import app
